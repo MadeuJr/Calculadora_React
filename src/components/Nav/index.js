55c18c66ac4a16af86852516468f40db1383422e
Amadeu José Andrade Junior <amadeu.junior@aluno.ifsp.edu.br>
2023-08-04 16:12:55 -0300

    Preparação do ambiente

---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <h2>Calculardora React</h2>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
